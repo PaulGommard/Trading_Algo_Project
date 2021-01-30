09dec0321b3f085d491b58de8ee4bc0ab4885980
Paul_Gommard <57675875+PaulGommard@users.noreply.github.com>
2021-01-31 00:19:34 +0100

    Recuperation data prix des stocks

--- create_db.py
+++ create_db.py
@@ -4,14 +4,15 @@ connection = sqlite3.connect('app.db')
 
 cursor = connection.cursor()
 
+# Stock table
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS stock ( 
         id INTEGER PRIMARY KEY, 
         symbol TEXT NOT NULL UNIQUE, 
-        company TEXT NOT NULL
+        name TEXT NOT NULL
     )
 """)
-
+# Stock_price table
 cursor.execute("""
     CREATE TABLE IF NOT EXISTS stock_price ( 
         id INTEGER PRIMARY KEY, 
@@ -21,7 +22,6 @@ cursor.execute("""
         high NOT NULL, 
         low NOT NULL,
         close NOT NULL, 
-        adjusted_close NOT NULL, 
         volume NOT NULL, 
         FOREIGN KEY (stock_id) REFERENCES stock (id)
     )
--- populate_db.py
+++ populate_db.py
@@ -1,29 +1,31 @@
 import sqlite3, config
 import alpaca_trade_api as tradeapi
 
+# Get the app data already created
 connection = sqlite3.connect('app.db')
 connection.row_factory = sqlite3.Row
 
+# Create connection
 cursor = connection.cursor()
 
+# Get symbol and company from the database
 cursor.execute("""SELECT symbol, company FROM stock""")
 rows = cursor.fetchall()
 symbols = [row['symbol'] for row in rows]
 
-
+# Create connection with Alpaca
 api = tradeapi.REST(config.API_KEY,config.SECRET_KEY, base_url=config.BASE_URL)
 assets = api.list_assets()
 
-def GetListStocks():
-    for asset in assets:
-        try:
-            if asset.status == 'active' and asset.tradable:
-                print(f"Added a new stock {asset.symbol} {asset.name}")
-                cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
-        except Exception as e:
-            print(asset.symbol)
-            print(e)
+# Insert the new stock in the db
+for asset in assets:
+    try:
+        if asset.status == 'active' and asset.tradable:
+            print(f"Added a new stock {asset.symbol} {asset.name}")
+            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+    except Exception as e:
+        print(asset.symbol)
+        print(e)
 
-GetListStocks()
 
 connection.commit()
